66cb612f82b6c8ae4faf2854a172b0673795f9c9
lcfr <me@lcfr.io>
2025-03-20 01:01:20 -0400

    chore: resolve merge conflicts

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1742444584',resources: [
+    cacheName: 'chirpy-1742446749',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
